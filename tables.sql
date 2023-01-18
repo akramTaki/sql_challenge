c5d461505b4ebac3303fe17cf71966ffb6750973
akramTaki <46896146+akramTaki@users.noreply.github.com>
2023-01-17 23:07:59 -0500

    homework submission

---
+++ tables.sql
@@ -0,0 +1,29 @@
+CREATE TABLE Merchant (
+	id int PRIMARY KEY,
+	name varchar(50) NOT NULL,
+	id_merchant_category int NOT NULL,
+		FOREIGN KEY (id_merchant_category) REFERENCES Merchant_Category(id))
+
+CREATE TABLE Cardholder (
+	id int PRIMARY KEY,
+	name varchar(50) NOT NULL)
+	
+CREATE TABLE Credit_Card (
+	card varchar(20) PRIMARY KEY,
+	cardholder_id int NOT NULL,
+		FOREIGN KEY (cardholder_id) REFERENCES Cardholder(id))
+	
+CREATE TABLE Merchant_Category (
+	id int PRIMARY KEY,
+	name varchar(50) NOT NULL)
+	
+
+	
+CREATE TABLE Transaction (
+	id int PRIMARY KEY,
+	date timestamp NOT NULL,
+	amount float NOT NULL,
+	card varchar(20) NOT NULL,
+		FOREIGN KEY (card) REFERENCES Credit_Card(card),
+	id_merchant int NOT NULL,
+		FOREIGN KEY (id_merchant) REFERENCES Merchant(id))
